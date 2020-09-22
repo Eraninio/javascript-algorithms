0e141d311ad3bb8dc48ef98a82cf604020b2aa2c
Eraninio <67857483+Eraninio@users.noreply.github.com>
2020-09-22 09:26:23 +0300

    finish

--- src/data-structures/priority-queue/newPriorityQueue.js
+++ src/data-structures/priority-queue/newPriorityQueue.js
@@ -1,6 +1,58 @@
-
-export default class PriorityQueue {
-  constructor() {
-
+export default class PriorityQueue{
+    constructor() {
+      this.collection = [];
+    }
+  
+    isEmpty() {
+      return this.collection.length === 0;
+    }
+  
+    add(item, priority = 0) {
+      if(this.isEmpty()) {
+        return this.collection = [[item, priority]];
+      }
+      for (let i = 0; i < this.collection.length; i++) {
+        if (this.collection[i][1] <= priority) {
+          this.collection.splice(i, 0, [item, priority]);
+          return this.collection;
+        }
+      }
+      this.collection.push([item, priority]);
+      return this.collection;
+    }
+  
+    peek() {
+      console.log(this.collection);
+      return this.isEmpty() ? null : this.collection[this.collection.length - 1][0];
+    }
+  
+    poll() {
+      return this.isEmpty() ? null : this.collection.pop()[0];
+    }
+  
+    changePriority(item, priority = 0) {
+      if(this.isEmpty()) {
+        return null;
+      }
+      for (let i = 0; i < this.collection.length; i++) {
+        if (this.collection[i][0] === item) {
+          this.collection.splice(i, 1);
+          this.add(item, priority);
+          return this.collection;
+        }
+      }
+      return null;
+    }
+  
+    hasValue(item) {
+      if(this.isEmpty()) {
+        return false;
+      }
+      for (let i = 0; i < this.collection.length; i++) {
+        if (this.collection[i][0] === item) {
+          return true;
+        }
+      }
+      return false;
+    }
   }
-}
